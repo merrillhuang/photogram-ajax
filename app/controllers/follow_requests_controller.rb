0833510f697ccf4014b53341b4b1b73b4a60a42b
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-10-07 19:43:50 +0000

    add handling of js format requests in follow_requests delete method

--- app/controllers/follow_requests_controller.rb
+++ app/controllers/follow_requests_controller.rb
@@ -54,6 +54,7 @@ class FollowRequestsController < ApplicationController
     respond_to do |format|
       format.html { redirect_back fallback_location: root_url, notice: "Follow request was successfully destroyed." }
       format.json { head :no_content }
+      format.js
     end
   end
 
